261d6d44323ae17ecd411baff82e9c744f45c72a
MedadRufus <medadrufus@gmail.com>
2019-11-12 22:22:47 +0000

    put in the first keil test projects

---
+++ prototype/firmware/Keil tests/MDK/Boards/ST/STM32L053-Discovery/Blinky/RTE/_Release/RTE_Components.h
@@ -0,0 +1,29 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'Release' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.1 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.1 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.10.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+
+
+#endif /* RTE_COMPONENTS_H */
