9db86bc060ab92d1f5230cb7eabca791faabbde7
MedadRufus <medadrufus@gmail.com>
2019-11-16 04:29:46 +0000

    put in a blink project that works just for stm32l082cz. removed all other test code

---
+++ prototype/firmware/Keil tests/Pico tracker lora/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Pico tracker lora' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.10.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+
+
+#endif /* RTE_COMPONENTS_H */
