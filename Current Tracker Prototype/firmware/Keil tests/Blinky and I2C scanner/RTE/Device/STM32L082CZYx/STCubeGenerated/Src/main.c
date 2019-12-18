fbe9b1694775483f52490f09648751017a99b393
MedadRufus <medadrufus@gmail.com>
2019-12-18 22:05:15 +0000

    update main.c in the blinky i2c scanner code

--- Current Tracker Prototype/firmware/Keil tests/Blinky and I2C scanner/RTE/Device/STM32L082CZYx/STCubeGenerated/Src/main.c
+++ Current Tracker Prototype/firmware/Keil tests/Blinky and I2C scanner/RTE/Device/STM32L082CZYx/STCubeGenerated/Src/main.c
@@ -130,58 +130,50 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
     
-    char uart2Data[24] = "Connected to UART One\r\n";
-    /*
-    * Output to uart2
-    * use screen or putty or whatever terminal software
-    * 8N1 115200
-    */
-    HAL_UART_Transmit(&huart1, (uint8_t *)&uart2Data,sizeof(uart2Data), HAL_MAX_DELAY);
+	char uart2Data[24] = "Connected to UART One\r\n";
+	/*
+	* Output to uart2
+	* use screen or putty or whatever terminal software
+	* 8N1 115200
+	*/
+	HAL_UART_Transmit(&huart1, (uint8_t *)&uart2Data,sizeof(uart2Data), HAL_MAX_DELAY);
 
-    printf("\r\n");
+	printf("\r\n");
 
-    printf("Scanning I2C bus:\r\n");
-    HAL_StatusTypeDef result;
-    uint8_t i;
-		while(1){
-    for (i=1; i<128; i++)
-    {
-        /*
-         * the HAL wants a left aligned i2c address
-         * &hi2c1 is the handle
-         * (uint16_t)(i<<1) is the i2c address left aligned
-         * retries 2
-         * timeout 2
-         */
-        result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i<<1), 20, 20);
-        if (result != HAL_OK) // HAL_ERROR or HAL_BUSY or HAL_TIMEOUT
-						
-        {
-            printf("."); // No ACK received at that address
-        }
-        if (result == HAL_OK)
-        {
-            printf("0x%X", i); // Received an ACK at that address
-        }
-    }
-    printf("\r\n");
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
-
-	}
-
+	printf("Scanning I2C bus:\r\n");
+	HAL_StatusTypeDef result;
+	uint8_t i;
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
-    HAL_Delay(100);                                /* Delay 1000ms                */
-     //HAL_UART_Transmit(&huart1, (uint8_t*)"printing_direct_uart\n", strlen("printing_direct_uart\n"), HAL_MAX_DELAY);
-    //printf("printing fing\n");
-        
-        //main_old();
+		while(1)
+			{
+				for (i=1; i<128; i++)
+				{
+					/*
+					 * the HAL wants a left aligned i2c address
+					 * &hi2c1 is the handle
+					 * (uint16_t)(i<<1) is the i2c address left aligned
+					 * retries 2
+					 * timeout 2
+					 */
+					result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i<<1), 20, 20);
+					if (result != HAL_OK) // HAL_ERROR or HAL_BUSY or HAL_TIMEOUT
+							
+					{
+							printf("."); // No ACK received at that address
+					}
+					if (result == HAL_OK)
+					{
+							printf("0x%X", i); // Received an ACK at that address
+					}
+    }
+    printf("\r\n");
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
