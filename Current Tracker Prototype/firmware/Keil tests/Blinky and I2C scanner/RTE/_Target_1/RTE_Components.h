7ae87ba1624a8fd31ea61a6e41da4f197a117063
MedadRufus <medadrufus@gmail.com>
2019-12-18 21:40:33 +0000

    put in a second project only blinky and i2c scanner

---
+++ Current Tracker Prototype/firmware/Keil tests/Blinky and I2C scanner/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,47 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky and I2C scanner' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+/*  Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.10.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+/*  Keil::CMSIS Driver:USART:USART Async:1.1.0 */
+#define RTE_Drivers_USART1_Async        /* Driver USART1 */
+        #define RTE_Drivers_USART2_Async        /* Driver USART2 */
+        #define RTE_Drivers_USART4_Async        /* Driver USART4 */
+        #define RTE_Drivers_USART5_Async        /* Driver USART5 */
+/*  Keil::CMSIS Driver:USART:USART IrDA:1.0.0 */
+#define RTE_Drivers_USART1_IrDA         /* Driver USART1 */
+        #define RTE_Drivers_USART2_IrDA         /* Driver USART2 */
+/*  Keil::CMSIS Driver:USART:USART SmartCard:1.0.0 */
+#define RTE_Drivers_USART1_SmartCard    /* Driver USART1 */
+        #define RTE_Drivers_USART2_SmartCard    /* Driver USART2 */
+/*  Keil::CMSIS Driver:USART:USART Sync:1.0.0 */
+#define RTE_Drivers_USART1_Sync         /* Driver USART1 */
+        #define RTE_Drivers_USART2_Sync         /* Driver USART2 */
+        #define RTE_Drivers_USART4_Sync         /* Driver USART4 */
+        #define RTE_Drivers_USART5_Sync         /* Driver USART5 */
+
+
+#endif /* RTE_COMPONENTS_H */
