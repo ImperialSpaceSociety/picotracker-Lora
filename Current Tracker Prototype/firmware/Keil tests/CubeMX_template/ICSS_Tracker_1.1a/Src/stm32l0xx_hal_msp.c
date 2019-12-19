09ea302dcaa8c5f414485420df5afa9cea1177ca
ribbotson <richard.ibbotson@btinternet.com>
2019-12-19 21:19:45 +0000

    Added STM32 CubeMX template

--- Current Tracker Prototype/firmware/Keil tests/CubeMX_template/ICSS_Tracker_1.1a/Src/stm32l0xx_hal_msp.c
+++ Current Tracker Prototype/firmware/Keil tests/CubeMX_template/ICSS_Tracker_1.1a/Src/stm32l0xx_hal_msp.c
@@ -156,20 +156,13 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
     __HAL_RCC_GPIOB_CLK_ENABLE();
     /**I2C1 GPIO Configuration    
     PB9     ------> I2C1_SDA
-    PB6     ------> I2C1_SCL 
+    PB8     ------> I2C1_SCL 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_6;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF1_I2C1;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* Peripheral clock enable */
@@ -199,9 +192,9 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
   
     /**I2C1 GPIO Configuration    
     PB9     ------> I2C1_SDA
-    PB6     ------> I2C1_SCL 
+    PB8     ------> I2C1_SCL 
     */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_9|GPIO_PIN_6);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_9|GPIO_PIN_8);
 
   /* USER CODE BEGIN I2C1_MspDeInit 1 */
 
