22d2d47b89a67810636690d58e290e53e384cdb3
MedadRufus <medadrufus@gmail.com>
2020-03-16 18:01:51 +0000

    set default data rate to DR_4

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/inc/main.h
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/inc/main.h
@@ -147,7 +147,7 @@ void Error_Handler(void);
  * LoRaWAN Default data Rate Data Rate
  * @note Please note that LORAWAN_DEFAULT_DATA_RATE is used only when ADR is disabled 
  */
-#define LORAWAN_DEFAULT_DATA_RATE DR_3
+#define LORAWAN_DEFAULT_DATA_RATE DR_4
 
 
 
