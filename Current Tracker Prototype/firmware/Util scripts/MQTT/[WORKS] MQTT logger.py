4c1f7cbe891c9d6eeb5467804fd52ceac263f1e1
MedadRufus <medadrufus@gmail.com>
2020-03-24 02:12:38 +0000

    made mqtt logger work
    cleaned up the files that don't work

--- Current Tracker Prototype/firmware/Util scripts/MQTT/[WORKS] MQTT logger.py
+++ Current Tracker Prototype/firmware/Util scripts/MQTT/[WORKS] MQTT logger.py
@@ -1,43 +1,43 @@
-# Created by Medad Rufus Newman on 18/03/2020
-
-
 # https://www.thethingsnetwork.org/forum/t/a-python-program-to-listen-to-your-devices-with-mqtt/9036/6
 # Get data from MQTT server
 # Run this with python 3, install paho.mqtt prior to use
 
 import paho.mqtt.client as mqtt
-import json
 import base64
+import json
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("mqtt.log"),
+        logging.StreamHandler()
+    ]
+)
+#APPEUI = "70B3D57EF00069A3"
 APPID  = "icss_lora_tracker"
-PSW    = "ttn-account-v2.jCAIrrrJhQO4UwJtYJVNhxzwkAobQLWkNGUvBS7eung"
+PSW    = 'ttn-account-v2.vlMjFic1AU9Dr-bAI18X6kzc5lSJGbFoeLbbASramBg'
 
 #Call back functions
 
 # gives connection message
-def on_connect(self, client, userdata, flags, rc):
-    logging.debug("MQTT connected: result code=%i", rc)
+def on_connect(mqttc, mosq, obj,rc):
+    print("Connected with result code:"+str(rc))
     if rc == 0:
-        res = client.subscribe("+/devices/+/up")
+        # subscribe for all devices of user
+        res_1 = mqttc.subscribe('+/devices/+/up')
+        res = mqttc.subscribe('+/devices/+/events/#')
+
         if res[0] != mqtt.MQTT_ERR_SUCCESS:
             raise RuntimeError("the client is not connected")
 
-    if rc == 1:
-        raise RuntimeError("connection failed: incorrect protocol version")
-    if rc == 2:
-        raise RuntimeError("connection failed: invalid client identifier")
-    if rc == 3:
-        raise RuntimeError("connection failed: server unavailable")
-    if rc == 4:
-        raise RuntimeError("connection failed: bad app_id or access_key")
-    if rc == 5:
-        raise RuntimeError("connection failed: not authorised")
-    # 6 - 255: currently unused
+
 # gives message from device
 def on_message(mqttc,obj,msg):
     try:
         #print(msg.payload)
+        logging.info(msg.payload)
         x = json.loads(msg.payload.decode('utf-8'))
         device = x["dev_id"]
         counter = x["counter"]
@@ -49,7 +49,7 @@ def on_message(mqttc,obj,msg):
         for gw in gateways:
             gateway_id = gw["gtw_id"]
             rssi = gw["rssi"]
-            print(datetime + ", " + device + ", " + str(counter) + ", "+ gateway_id + ", "+ str(rssi) + ", " + str(payload_fields))
+            #print(datetime + ", " + device + ", " + str(counter) + ", "+ gateway_id + ", "+ str(rssi) + ", " + str(payload_fields))
     except Exception as e:
         print(e)
         pass
@@ -60,18 +60,17 @@ def on_publish(mosq, obj, mid):
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-def on_log(self, client, userdata, level, buf):
-    """
-    Log all MQTT protocol events, and the exceptions in callbacks
-    that have been caught by Paho.
-    """
-    logging_level = mqtt.LOGGING_LEVEL[level]
-    logging.log(logging_level, buf)
+def on_log(mqttc,obj,level,buf):
+    print("message:" + str(buf))
+    print("userdata:" + str(obj))
 
 mqttc= mqtt.Client()
 # Assign event callbacks
 mqttc.on_connect=on_connect
 mqttc.on_message=on_message
+mqttc.enable_logger(logger=None)
+mqttc.reconnect_delay_set(min_delay=1, max_delay=120)
+
 
 mqttc.username_pw_set(APPID, PSW)
 mqttc.connect("eu.thethings.network",1883,60)
