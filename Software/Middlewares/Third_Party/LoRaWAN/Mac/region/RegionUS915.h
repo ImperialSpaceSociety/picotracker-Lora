38cefe7c9da4a7b744147518400ba18a6275974d
Medad Newman <medadrufus@gmail.com>
2020-07-03 23:50:23 +0100

    reset min datarates in us and au to loramac project

--- Software/Middlewares/Third_Party/LoRaWAN/Mac/region/RegionUS915.h
+++ Software/Middlewares/Third_Party/LoRaWAN/Mac/region/RegionUS915.h
@@ -47,7 +47,7 @@
 /*!
  * Minimal datarate that can be used by the node
  */
-#define US915_TX_MIN_DATARATE                       DR_4
+#define US915_TX_MIN_DATARATE                       DR_0
 
 /*!
  * Maximal datarate that can be used by the node
