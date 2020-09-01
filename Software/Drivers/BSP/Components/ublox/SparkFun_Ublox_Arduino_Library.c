0119ed026199c8007c78ffe0f25060d124b8d8e9
Medad Newman <medadrufus@gmail.com>
2020-09-01 23:05:03 +0100

    Revert "Do printout nema from GPS if it comes through"
    This reverts commit 6441bf7145dea3dfca4785e70d0ef496b9007460.

--- Software/Drivers/BSP/Components/ublox/SparkFun_Ublox_Arduino_Library.c
+++ Software/Drivers/BSP/Components/ublox/SparkFun_Ublox_Arduino_Library.c
@@ -457,7 +457,9 @@ void process(uint8_t incoming, ubxPacket *incomingUBX, uint8_t requestedClass, u
 //Or user could pipe each character to a buffer, radio, etc.
 void processNMEA(char incoming)
 {
-	PRINTF("%c",incoming); //Echo this byte to the serial port
+//  //If user has assigned an output port then pipe the characters there
+//  if (_nmeaOutputPort != NULL)
+    //PRINTF("%c",incoming); //Echo this byte to the serial port
 }
 
 //We need to be able to identify an RTCM packet and then the length
