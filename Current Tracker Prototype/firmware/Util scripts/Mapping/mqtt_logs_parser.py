2bc9b50ebcc64fc1d844a2206d3b2915e508e34d
MedadRufus <mailto@medadnewman.co.uk>
2020-11-16 23:04:34 +0000

    plot the diffs

--- Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
+++ Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
@@ -7,8 +7,10 @@
 
 import re
 from pathlib import Path
-
-
+import json
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
 
 line_selector = "\[(Sun|Mon|Tues|Wed|Thurs|Fri|Sat), (\d{2}) (Jan|Feb|Mar|Apr|May|June|July|Aug|Sep|Oct|Nov|Dec) (\d{4}) ([0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\] INFO b'(.*)'"
 
@@ -22,8 +24,24 @@ lines = re.findall(line_selector, raw_data)
 
 #print(*lines, sep="\n")
 
-
+timestamps = []
 for i in lines:
     day, date, month, year, hour, minute, second, data = i
-    print(data)
+    d = json.loads(data)
+    try:
+        if d["dev_id"] == "icspace22":
+            ##print(d["metadata"]["time"].replace("Z",""))
+            ##timestamps.append(datetime.strptime(d["metadata"]["time"][:-4], '%Y-%m-%dT%H:%M:%S.%f'))
+            timestamps.append(d["metadata"]["time"][:-4])
+    except KeyError:
+        pass
 
+
+df = pd.DataFrame(timestamps, columns = ["datetime"])
+df = pd.to_datetime(df['datetime'])
+diffs = df.diff().dt.total_seconds()
+
+#diffs = diffs[diffs[""] > 0]
+
+plt.scatter(x = df,y=diffs)
+plt.show()
