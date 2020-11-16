a9e86d1ef2df5cd851fc3a9ed63b81b1ac784982
MedadRufus <mailto@medadnewman.co.uk>
2020-11-16 21:44:28 +0000

    put in mqtt log parser regex

--- Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
+++ Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
@@ -10,14 +10,14 @@ from pathlib import Path
 
 
 
-line_selector = '([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9]),([0-9]{3}) (.*)'
+line_selector = '\[(Sun|Mon|Tues|Wed|Thurs|Fri|Sat), (\d{2}) (Jan|Feb|Mar|Apr|May|June|July|Aug|Sep|Oct|Nov|Dec) (\d{4}) ([0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\] (.*)'
 
 
-filename = "ICSPACE20-21.out"
+filename = "mqtt_log_data-1.txt"
 
 raw_data = Path(filename).read_text()
 
 lines = re.findall(line_selector, raw_data)
 
 
-print(lines, sep="\n")
+print(*lines, sep="\n")
