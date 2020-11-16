eb5402fb58f5ac3f60e2e6e2d1267f5cebd0bc04
MedadRufus <mailto@medadnewman.co.uk>
2020-11-16 21:53:37 +0000

    update regex arguments

--- Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
+++ Current Tracker Prototype/firmware/Util scripts/Mapping/mqtt_logs_parser.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 
 
-line_selector = '\[(Sun|Mon|Tues|Wed|Thurs|Fri|Sat), (\d{2}) (Jan|Feb|Mar|Apr|May|June|July|Aug|Sep|Oct|Nov|Dec) (\d{4}) ([0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\] (.*)'
+line_selector = "\[(Sun|Mon|Tues|Wed|Thurs|Fri|Sat), (\d{2}) (Jan|Feb|Mar|Apr|May|June|July|Aug|Sep|Oct|Nov|Dec) (\d{4}) ([0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\] INFO b'(.*)'"
 
 
 filename = "mqtt_log_data-1.txt"
@@ -20,4 +20,10 @@ raw_data = Path(filename).read_text()
 lines = re.findall(line_selector, raw_data)
 
 
-print(*lines, sep="\n")
+#print(*lines, sep="\n")
+
+
+for i in lines:
+    day, date, month, year, hour, minute, second, data = i
+    print(data)
+
