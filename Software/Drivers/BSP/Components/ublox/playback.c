2dc53ff7befa99119abc7de97a3cd764f380c0c1
MedadRufus <mailto@medadnewman.co.uk>
2020-11-21 03:26:16 +0000

    use pointer correctly

--- Software/Drivers/BSP/Components/ublox/playback.c
+++ Software/Drivers/BSP/Components/ublox/playback.c
@@ -253,22 +253,22 @@ void prepare_tx_buffer(void)
 {
 	  
 	  /* byte 0: no load voltage(5 bits) and load voltage(3 bits) */
-	  tx_str_buffer[0] |= ((current_sensor_data.no_load_solar_voltage - 18) & 0x1F) << 3;
-	  tx_str_buffer[0] |= ((current_sensor_data.load_solar_voltage - 18) & 0x1C) >> 2;
+	  tx_str_buffer[0] |= ((sensor_data_ptr->no_load_solar_voltage - 18) & 0x1F) << 3;
+	  tx_str_buffer[0] |= ((sensor_data_ptr->load_solar_voltage - 18) & 0x1C) >> 2;
 	  
 	  /* byte1: load voltage(remaining 2 bits) and temperature(6 bits)*/
-	  tx_str_buffer[1] |= ((current_sensor_data.load_solar_voltage - 18) & 0x03) << 6;
-	  tx_str_buffer[1] |= (current_sensor_data.temperature >> 2 & 0x3F);
+	  tx_str_buffer[1] |= ((sensor_data_ptr->load_solar_voltage - 18) & 0x03) << 6;
+	  tx_str_buffer[1] |= (sensor_data_ptr->temperature >> 2 & 0x3F);
 	  /* byte2: pressure(7 bits) and data received flag(1 bit)*/
-	  tx_str_buffer[2] |= ((current_sensor_data.pressure/10) & 0x7F) << 1;
-	  tx_str_buffer[2] |=	(current_sensor_data.data_received & 0x01);
+	  tx_str_buffer[2] |= ((sensor_data_ptr->pressure/10) & 0x7F) << 1; // TODO: some minusing has to happen for pressure
+	  tx_str_buffer[2] |=	(sensor_data_ptr->data_received & 0x01);
 	  
 	  /* byte3: Sats(5 bits) and reset count(3 bits)*/
-	  tx_str_buffer[3] |= (current_sensor_data.sats & 0x1F) << 3;
-	  tx_str_buffer[3] |= (current_sensor_data.reset_count & 0x07);
+	  tx_str_buffer[3] |= (sensor_data_ptr->sats & 0x1F) << 3;
+	  tx_str_buffer[3] |= (sensor_data_ptr->reset_count & 0x07);
 
 	  
-	  fill_tx_buffer_with_location(4, tx_str_buffer, current_pos.latitude,current_pos.longitude,current_pos.altitude);
+	  fill_tx_buffer_with_location(4, tx_str_buffer, current_pos_ptr->latitude,current_pos_ptr->longitude,current_pos_ptr->altitude);
 
 	  
 	  for (int i = 0; i < current_playback_key_info.n_positions_to_send; i++)
