a3261bbd7fc564bbccd3438aa56beeb997b89d26
MedadRufus <mailto@medadnewman.co.uk>
2020-11-26 20:40:26 +0000

    Update generate_config_instructions.py

--- Current Tracker Prototype/firmware/Util scripts/RNG/generate_config_instructions.py
+++ Current Tracker Prototype/firmware/Util scripts/RNG/generate_config_instructions.py
@@ -2,8 +2,6 @@ import struct
 import time
 from datetime import datetime
 
-time.time()
-
 
 epoch_unix = 1577840461
 
@@ -27,15 +25,12 @@ def convert_special_epoch_min_to_datetime(ts_min:int)->str:
     return datetime.utcfromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
-
-    date_time_str = '2020-11-26 11:30:27.243860'
+    date_time_str = '2020-11-26 19:30:57.084'
     date_time_older = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
 
-    date_time_str = '2020-11-26 11:50:27.243860'
+    date_time_str = '2020-11-26 19:42:03.972'
     date_time_newer = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
 
 
     print(gen_instruction(convert_datatime_to_special_epoch_min(date_time_older),
                           convert_datatime_to_special_epoch_min(date_time_newer)))
-
-    print(convert_special_epoch_min_to_datetime(472110))
