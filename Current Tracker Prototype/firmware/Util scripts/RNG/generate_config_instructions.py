eb5d588985ecc6d6ad3c161293c854a0748534ba
MedadRufus <mailto@medadnewman.co.uk>
2020-11-27 00:55:12 +0000

    Update generate_config_instructions.py

--- Current Tracker Prototype/firmware/Util scripts/RNG/generate_config_instructions.py
+++ Current Tracker Prototype/firmware/Util scripts/RNG/generate_config_instructions.py
@@ -11,13 +11,18 @@ def convert_datatime_to_special_epoch_min(ts:datetime):
 
 
 def gen_instruction(older, newer):
-    print("older",older)
-    print("newer",newer)
+
+    if older>= newer:
+        raise ValueError("Older is equal or greater than newer")
     newer = struct.pack('<I', newer)
     older = struct.pack('<I', older)
 
 
     return newer.hex()+older.hex()
+
+def unix_to_datetime_str(unix):
+    return datetime.utcfromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')
+
 
 def convert_special_epoch_min_to_datetime(ts_min:int)->str:
     unix = ts_min*60+epoch_unix
@@ -25,12 +30,23 @@ def convert_special_epoch_min_to_datetime(ts_min:int)->str:
     return datetime.utcfromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
-    date_time_str = '2020-11-26 19:30:57.084'
-    date_time_older = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+    date_time_str = '2020-11-26 23:53:01'
+    date_time_older = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
 
-    date_time_str = '2020-11-26 19:42:03.972'
-    date_time_newer = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+    date_time_str = '2020-11-26 23:56:01'
+    date_time_newer = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
 
 
     print(gen_instruction(convert_datatime_to_special_epoch_min(date_time_older),
                           convert_datatime_to_special_epoch_min(date_time_newer)))
+
+    print(convert_special_epoch_min_to_datetime(476586))
+    print(convert_special_epoch_min_to_datetime(476584))
+    print(convert_special_epoch_min_to_datetime(476582))
+    print(convert_special_epoch_min_to_datetime(476580))
+    print(convert_special_epoch_min_to_datetime(476578))
+    print(convert_special_epoch_min_to_datetime(476576))
+    print(convert_special_epoch_min_to_datetime(476574))
+    print(convert_special_epoch_min_to_datetime(476572))
+    print(convert_special_epoch_min_to_datetime(476570))
+    print(convert_special_epoch_min_to_datetime(476568))
