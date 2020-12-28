19ab6840c9d053155e5a64b771e8e6fec37f7a28
MedadRufus <medadrufus@gmail.com>
2020-12-28 00:50:30 +0000

    increment counter after 20 minutes, then save (#51)

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/bsp.c
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/bsp.c
@@ -163,9 +163,6 @@ void BSP_sensor_Read(void)
 
 	if (gps_info.latest_gps_status == GPS_SUCCESS)
 	{
-		/* Save position to eeprom, overwriting the latest position with every fix. */
-		save_current_position_info_to_EEPROM(&current_position);
-		
 		/* After the time between saving(HOW_OFTEN_TO_SAVE_POS_TIM_TO_EEPROM) has elapsed, then
 		 * increment the counter such that it can save to the next location
 		 */
@@ -173,6 +170,11 @@ void BSP_sensor_Read(void)
 		{
 			playback_key_info_ptr->n_positions_saved_since_boot += 1;
 		}
+		
+		/* Save position to eeprom, overwriting the latest position with every fix. */
+		save_current_position_info_to_EEPROM(&current_position);
+		
+
 	}
 }
 
