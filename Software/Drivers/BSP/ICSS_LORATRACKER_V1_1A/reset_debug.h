a562cce791e12f2781f39c16d197403099bade63
MedadRufus <medadrufus@gmail.com>
2021-01-09 04:12:43 +0000

    Print reset cause (#55)
    * put in reset debug checker
    
    * move reset debug files
    
    * add reset debug files to project
    
    * put in reset case in the main.c
    
    * catch all reset causes
    
    * ban power saving configs for ublox
    
    * set gps fix timeout to 12 seconds
    
    * set reset cause check at the beginning
    
    * Revert "ban power saving configs for ublox"
    
    This reverts commit e9ef4cc4fa53b191b3ca5478ab9719000b76512d.
    
    * formating only
    
    Co-authored-by: MedadRufus <mailto@medadnewman.co.uk>

---
+++ Software/Drivers/BSP/ICSS_LORATRACKER_V1_1A/reset_debug.h
@@ -0,0 +1,65 @@
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef reset_debug_h
+#define reset_debug_h
+
+
+/* ==================================================================== */
+/* ========================== include files =========================== */
+/* ==================================================================== */
+
+/* Inclusion of system and local header files goes here */
+
+
+
+/* ==================================================================== */
+/* ============================ constants ============================= */
+/* ==================================================================== */
+
+/* #define and enum statements go here */
+
+
+
+/* ==================================================================== */
+/* ========================== public data ============================= */
+/* ==================================================================== */
+
+/* Definition of public (external) data types go here */
+
+/// @brief  Possible STM32 system reset causes
+typedef enum reset_cause_e
+{
+    RESET_CAUSE_UNKNOWN = 0,
+    RESET_CAUSE_LOW_POWER_RESET,
+    RESET_CAUSE_WINDOW_WATCHDOG_RESET,
+    RESET_CAUSE_INDEPENDENT_WATCHDOG_RESET,
+    RESET_CAUSE_SOFTWARE_RESET,
+    RESET_CAUSE_POWER_ON_POWER_DOWN_RESET,
+    RESET_CAUSE_EXTERNAL_RESET_PIN_RESET,
+    RESET_CAUSE_BROWNOUT_RESET,
+		RESET_CAUSE_OPTIONS_BYTES_LOADING_RESET,
+		RESET_CAUSE_FIREWALL_RESET
+} reset_cause_t;
+
+
+
+/* ==================================================================== */
+/* ======================= public functions =========================== */
+/* ==================================================================== */
+
+/* Function prototypes for public (external) functions go here */
+reset_cause_t reset_cause_get(void);
+const char * reset_cause_get_name(reset_cause_t reset_cause);
+
+
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+
