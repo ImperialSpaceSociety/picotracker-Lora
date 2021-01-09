51ce1351459acab3b7ace99b8ce17aaa44119dae
MedadRufus <mailto@medadnewman.co.uk>
2021-01-09 06:11:24 +0000

    intialise properly the iwdg

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/iwdg.c
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/iwdg.c
@@ -37,6 +37,7 @@ void MX_IWDG_Init(void)
   hiwdg.Instance = IWDG;
   hiwdg.Init.Prescaler = IWDG_PRESCALER_256;
   hiwdg.Init.Reload = 4095;
+	hiwdg.Init.Window = 4095;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
