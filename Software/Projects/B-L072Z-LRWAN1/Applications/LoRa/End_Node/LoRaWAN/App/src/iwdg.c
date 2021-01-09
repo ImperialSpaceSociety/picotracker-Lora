2f3b12b862eee3b7bb479754c5acd90559684169
MedadRufus <mailto@medadnewman.co.uk>
2021-01-09 04:00:50 +0000

    declare not implemented

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/iwdg.c
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/iwdg.c
@@ -49,14 +49,7 @@ void MX_IWDG_Init(void)
 /* IWDG init function */
 void MX_IWDG_DeInit(void)
 {
-  hiwdg.Instance = IWDG;
-  hiwdg.Init.Prescaler = IWDG_PRESCALER_256;
-  hiwdg.Init.Reload = 4095;
-  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
+	// Not implemented
 }
 
 /* USER CODE END 1 */
