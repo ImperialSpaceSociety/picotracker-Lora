f2712fed4d91175aa0560de3317d52eb16ee89cb
MedadRufus <mailto@medadnewman.co.uk>
2021-01-13 10:51:57 +0000

    update playback days to 15 days from 10

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/inc/main.h
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/inc/main.h
@@ -110,7 +110,7 @@ void Error_Handler(void);
 #define DEFAULT_N_POSITIONS_TO_SEND 13U             /* Number of past positions to send in each transmission */
 #define MAX_N_POSITIONS_TO_SEND 25U
 #define DEFAULT_N_POSITIONS_OFFSET 0U
-#define PLAYBACK_DAYS 10U                           /* How many days ago to send playback positions(default) */
+#define PLAYBACK_DAYS 15U                           /* How many days ago to send playback positions(default) */
 
 /* WATCHDOG RELATED DEFINES */
 /* ----------------------------------------------------------------------------------- */
