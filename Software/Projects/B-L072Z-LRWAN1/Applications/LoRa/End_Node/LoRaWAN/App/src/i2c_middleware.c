f9ed827c4047fbc2ede540bbf63f05d5d3b4c181
MedadRufus <medadrufus@gmail.com>
2021-04-20 19:26:39 +0100

    Revert "Auto format only"
    This reverts commit e9b203d2b29576782232648bc8378281234685cc.

--- Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/i2c_middleware.c
+++ Software/Projects/B-L072Z-LRWAN1/Applications/LoRa/End_Node/LoRaWAN/App/src/i2c_middleware.c
@@ -11,6 +11,7 @@
   ******************************************************************************
   */
 
+
 /* ==================================================================== */
 /* ========================== include files =========================== */
 /* ==================================================================== */
@@ -21,6 +22,7 @@
 #include "hw_i2c.h"
 #include "hw.h" // for PRINTF
 #include "main.h"
+
 
 /* ==================================================================== */
 /* ============================ constants ============================= */
@@ -34,11 +36,16 @@
 
 /* Global variables definitions go here */
 
+
+
 /* ==================================================================== */
 /* ========================== private data ============================ */
 /* ==================================================================== */
 
 /* Definition of private datatypes go here */
+
+
+
 
 /* ==================================================================== */
 /* ====================== private functions =========================== */
@@ -46,8 +53,12 @@
 
 /* Function prototypes for private (static) functions go here */
 
+
 void I2C_pins_GPIO_OUTPUT_init(void);
 void I2C_pins_GPIO_INPUT_init(void);
+
+
+
 
 /* ==================================================================== */
 /* ===================== All functions by section ===================== */
@@ -55,53 +66,63 @@ void I2C_pins_GPIO_INPUT_init(void);
 
 /* Functions definitions go here, organised into sections */
 
-I2C_MIDDLEWARE_STATUS_t I2C_receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+I2C_MIDDLEWARE_STATUS_t I2C_receive(I2C_HandleTypeDef* hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout )
 {
 
-	if (HAL_I2C_Master_Receive(hi2c, DevAddress, pData, Size, Timeout) != HAL_OK)
-	{
-		/* Error_Handler() function is called when error occurs. */
-	}
+		if(HAL_I2C_Master_Receive(hi2c, DevAddress, pData, Size,Timeout)!= HAL_OK)
+		{
+			/* Error_Handler() function is called when error occurs. */
+		}
 
-	if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE)
-	{
-		return I2C_SUCCSS;
-	}
 
+
+
+		if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE){
+				return I2C_SUCCSS;
+		}
+	
 	return I2C_FAIL;
-}
+}	
+
 
 /*
     Transmits a desired UBX message across I2C1.
 */
-I2C_MIDDLEWARE_STATUS_t I2C_transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+I2C_MIDDLEWARE_STATUS_t I2C_transmit(I2C_HandleTypeDef* hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout )
 {
 
-	if (HAL_I2C_Master_Transmit(hi2c, DevAddress, pData, Size, Timeout) != HAL_OK)
-	{
-		/* Error_Handler() function is called when error occurs. */
-	}
+		if(HAL_I2C_Master_Transmit(hi2c, DevAddress, pData, Size,Timeout)!= HAL_OK)
+		{
+			/* Error_Handler() function is called when error occurs. */
+		}
 
-	if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE)
-	{
-		return I2C_SUCCSS;
-	}
 
+
+		if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE){
+				return I2C_SUCCSS;
+		}
+
+	
 	return I2C_FAIL;
 }
 
-I2C_MIDDLEWARE_STATUS_t I2C_receive_mem(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+
+I2C_MIDDLEWARE_STATUS_t I2C_receive_mem(I2C_HandleTypeDef* hi2c, uint16_t DevAddress, uint16_t MemAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout )
 {
 
-	if (HAL_I2C_Mem_Read(hi2c, DevAddress, MemAddress, I2C_MEMADD_SIZE_8BIT, pData, Size, Timeout) != HAL_OK)
-	{
-	}
 
-	if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE)
-	{
-		return I2C_SUCCSS;
-	}
+		if(HAL_I2C_Mem_Read(hi2c, DevAddress,MemAddress,I2C_MEMADD_SIZE_8BIT, pData, Size,Timeout) != HAL_OK)
+		{
+			
+		}
 
+
+
+		if (HAL_I2C_GetError(hi2c) == HAL_I2C_ERROR_NONE){
+				return I2C_SUCCSS;
+		}
+	
 	return I2C_FAIL;
 }
 
@@ -109,49 +130,49 @@ I2C_MIDDLEWARE_STATUS_t I2C_receive_mem(I2C_HandleTypeDef *hi2c, uint16_t DevAdd
  * Workaround to solve this mysterious problem where the sda line
  * appears to get stuck low.
  */
-I2C_MIDDLEWARE_STATUS_t reinit_i2c(I2C_HandleTypeDef *hi2c)
+I2C_MIDDLEWARE_STATUS_t reinit_i2c(I2C_HandleTypeDef* hi2c)
 {
-	PRINTF("I2C ERROR!");
+	PRINTF("I2C ERROR!");	
 
-	/* Re-Initiaize the I2C comunication bus */
+	/* Re-Initiaize the I2C comunication bus */	
 	HAL_StatusTypeDef status = HAL_I2C_DeInit(hi2c);
+	
+	
+	switch(status) {
+		
+		case HAL_ERROR:
+		PRINTF("HAL_ERROR deinit i2c error\n");	
+		break; 
 
-	switch (status)
-	{
+		case HAL_OK:
+		PRINTF("HAL_OK deinit\n");	
+		break; 
 
-	case HAL_ERROR:
-		PRINTF("HAL_ERROR deinit i2c error\n");
-		break;
-
-	case HAL_OK:
-		PRINTF("HAL_OK deinit\n");
-		break;
-
-	/* you can have any number of case statements */
-	default:
-		PRINTF("unknown deinit i2c error\n");
+		/* you can have any number of case statements */
+		default: 
+		PRINTF("unknown deinit i2c error\n");	
 	}
-
+	
 	HAL_Delay(20);
-
+	
+	
 	status = HAL_I2C_Init(hi2c);
+	
+	switch(status) {
+		
+		case HAL_ERROR:
+		PRINTF("HAL_ERROR init i2c error\n");	
+		break; 
 
-	switch (status)
-	{
+		case HAL_OK:
+		PRINTF("HAL_OK init\n");	
+		break; 
 
-	case HAL_ERROR:
-		PRINTF("HAL_ERROR init i2c error\n");
-		break;
-
-	case HAL_OK:
-		PRINTF("HAL_OK init\n");
-		break;
-
-	/* you can have any number of case statements */
-	default:
-		PRINTF("unknown init i2c error\n");
+		/* you can have any number of case statements */
+		default: 
+		PRINTF("unknown init i2c error\n");	
 	}
-
+	
 	HAL_IWDG_Refresh(&hiwdg);
 
 	return I2C_SUCCSS;
