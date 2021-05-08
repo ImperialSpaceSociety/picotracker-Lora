5dff64ada9eed1278a1a7ec15a0b88cb5effb61e
MedadRufus <medadrufus@gmail.com>
2021-05-08 22:17:42 +0100

    setup files for region settings

---
+++ Software/Drivers/BSP/Components/ublox/region_setting.h
@@ -0,0 +1,50 @@
+/**
+ * @file region_setting.h
+ * @author Medad Rufus Newman (mailto@medadnewman.co.uk)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef REGION_SETTING_H
+#define REGION_SETTING_H
+
+	/* ==================================================================== */
+	/* ========================== include files =========================== */
+	/* ==================================================================== */
+
+	/* Inclusion of system and local header files goes here */
+
+#include "stdint.h"
+#include "LoRaMac.h"
+
+	/* ==================================================================== */
+	/* ============================ constants ============================= */
+	/* ==================================================================== */
+
+	/* #define and enum statements go here */
+
+	/* ==================================================================== */
+	/* ========================== public data ============================= */
+	/* ==================================================================== */
+
+	/* Definition of public (external) data types go here */
+
+	/* ==================================================================== */
+	/* ======================= public functions =========================== */
+	/* ==================================================================== */
+
+	/* Function prototypes for public (external) functions go here */
+
+#endif
+#ifdef __cplusplus
+}
+#endif
