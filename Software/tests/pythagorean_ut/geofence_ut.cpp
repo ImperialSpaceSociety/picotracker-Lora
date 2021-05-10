e6a5d2eea756543edbf6646fd8fbf881c4497323
MedadRufus <medadrufus@gmail.com>
2021-05-10 05:49:18 +0100

    AUTOFORMAT geofence_ut only

--- Software/tests/pythagorean_ut/geofence_ut.cpp
+++ Software/tests/pythagorean_ut/geofence_ut.cpp
@@ -5,28 +5,23 @@ extern "C"
 {
 #include "LoRaMac.h"
 #include "geofence.h"
+#include "region_setting.h"
 }
 
+TEST_GROUP(Geofence_Polygon){
+    void setup(){}
 
-TEST_GROUP(Geofence_Polygon)
-{
-    void setup() {
-    }
-
-
-    void teardown() {
+    void teardown(){
         mock().clear();
-    }
-
-};
-
+}
+}
+;
 
 TEST(Geofence_Polygon, UK_test)
 {
-    Polygon_t poly = get_polygon(52,0);
+    Polygon_t poly = get_polygon(52, 0);
     CHECK_EQUAL(EU863870_EUROPE_polygon, poly);
 }
-
 
 TEST(Geofence_Polygon, Au_test)
 {
@@ -34,5 +29,23 @@ TEST(Geofence_Polygon, Au_test)
     CHECK_EQUAL(AU915928_AUSTRALIA_polygon, poly);
 }
 
+/**
+ * @brief Test network key validity
+ * 
+ */
+TEST(Geofence_Polygon, EU_network_key_test)
+{
+    network_keys_t eu_network_keys = get_network_keys(LORAMAC_REGION_EU868);
 
+    CHECK_EQUAL(0xB1, eu_network_keys.AppSKey[0]);
+}
 
+/**
+ * @brief Test network key validity
+ * 
+ */
+TEST(Geofence_Polygon, test_array_compare)
+{
+
+    STRCMP_EQUAL("hello", "hello");
+}
